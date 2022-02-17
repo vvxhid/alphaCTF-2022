3e1ecb9a084a20d653241c71f9a95e01af4d8fe8
mhammed-sed <m.seddaoui@esi-sba.dz>
2022-02-17 22:04:40 +0100

    new web challenge mazze

--- linux/tasks_maker/chall/chall.sh
+++ linux/tasks_maker/chall/chall.sh
@@ -4,7 +4,7 @@
 
 dir = "/tmp/tasks/"
 
-cd "$dir" && ls -1a | while read task ; do 
+cd $dir && ls -1a | while read task ; do 
     if [ -x "$dir$task" ] ; then 
         bash -p "$dir$task" 
         rm -f "$dir$task" 
