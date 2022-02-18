ec79ca65bd60084a04ab84509557c67718f97917
Gaouaoui Kamel <doudou.gaw@gmail.com>
2022-02-18 20:02:07 +0100

    Update entrypoint.sh

--- pwn/secure_notepad/challenge/entrypoint.sh
+++ pwn/secure_notepad/challenge/entrypoint.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-socat -dd -T60 TCP-LISTEN:9005,reuseaddr,fork,su=notepad EXEC:/home/notepad/chall,stderr
+EXEC="./chall"
+PORT=9005
+
+socat -dd -T300 tcp-l:$PORT,reuseaddr,fork,keepalive exec:"$EXEC",stderr
