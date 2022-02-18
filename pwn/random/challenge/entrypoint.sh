e84046f2884ba2a390824bf21d3cc5a614747a28
Gaouaoui Kamel <doudou.gaw@gmail.com>
2022-02-18 20:27:39 +0100

    Update entrypoint.sh

--- pwn/random/challenge/entrypoint.sh
+++ pwn/random/challenge/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-EXEC="./chall"
 PORT=9004
 
-socat -dd -T300 tcp-l:$PORT,reuseaddr,fork,keepalive exec:"$EXEC",stderr
+socat -dd -T300 tcp-l:$PORT,reuseaddr,fork,keepalive exec:/home/random/chall,stderr
